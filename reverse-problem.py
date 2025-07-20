f1ed0bb09d851884f712cb51b5ae99eaa3ad5142
Praveenkanth G <praveenkanth2805@gmail.com>
2025-07-20 18:54:26 +0530

    add a code in the file

--- reverse-problem.py
+++ reverse-problem.py
@@ -1,1 +1,9 @@
-#this is a python file
+""" Quetion is reverse the number using only number not change the string """
+
+n  = int(input("give a number for reverse:"))
+r=0
+while n!=0:
+	m=n%10
+	n=n//1
+	r=r*10+m
+print("reversed number is:",r)
